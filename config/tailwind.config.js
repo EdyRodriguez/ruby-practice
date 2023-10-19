5c1d0ce73ea146cf4cbfc06b167eae1910ee0505
Edy Rodriguez <zeronoaz@gmail.com>
2023-10-19 14:43:53 -0600

    feat: added styles for article cards

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode : 'jit',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -8,7 +9,19 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#e8f3ff',
+        secondary: '#b3d4ff',
+        tertiary: '#112cff',
+        buttons: '#5689ff',
+        buttonsHover: '#10269f',
+        buttonsTitle: '#ffffff',
+        links: '#2f5cff',
+        letters: '#0a145c',
+        titles: '#000000',
+      },
+    },
   },
   plugins: []
 }
